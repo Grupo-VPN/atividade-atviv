3b49eca2271c4bceeeade501072015e5c39483d4
ZeroPirata <gabriel0gsbn@gmail.com>
2022-06-19 19:14:18 -0300

    Geração de Produtos e Servicos Iniciais

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import cors from 'cors'
 import clienteRoute from 'routes/cliente'
 import produtoRoute from 'routes/produto'
 import ServicoRoute from 'routes/servico'
+import { produtos } from "models/produto";
+import { servicos } from "models/servico";
 
 const corsOptions = {
     origin: '*',
@@ -15,6 +17,26 @@ const app = express();
 try {
     AppDataSource.initialize().then(async () => {
         console.log('Banco conectado com sucesso');
+        await AppDataSource.createQueryBuilder()
+            .insert()
+            .into(produtos)
+            .values([
+                { produto_nome: "Sabonete", produto_valor: 10.00 },
+                { produto_nome: "Creme de Cabelo", produto_valor: 20.00 },
+                { produto_nome: "Cortador de unhas", produto_valor: 5.00 },
+                { produto_nome: "Creme Dental", produto_valor: 7.50 },
+            ])
+            .execute()
+        await AppDataSource.createQueryBuilder()
+        .insert()
+        .into(servicos)
+        .values([
+            {servico_nome: "Cortar Cabelo", servico_valor: 50.75},
+            {servico_nome: "Manicure", servico_valor: 50.75},
+            {servico_nome: "Pedicure", servico_valor: 50.75},
+            {servico_nome: "Cabelo na régua", servico_valor: 50.75}
+        ])
+        .execute()
     })
 } catch (error) {
     console.log(`Connection error ${error}`);
